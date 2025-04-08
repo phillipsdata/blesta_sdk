76ddeec843cb554e4870ee339e55b2434eb49620
mlgtcode <git@mlgt.info>
2025-04-08 18:27:53 +0200

    start
    start

---
+++ api_v2/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Configuration for Blesta API
+ */
+return [
+    'url' => 'https://example.com/api/',
+    'user' => '',
+    'key' => '',
+    'ssl_verify' => true, // Set to false to disable SSL verification
+    'debug' => false,     // Set to true to enable debug mode
+];
+?>
